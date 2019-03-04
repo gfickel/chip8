1171a8c0133c69015d7b03591c16b04b736a610d
Guilherme Fickel <guilhermefickel@gmail.com>
2019-03-04 12:06:27 -0300

    Adding a main.cpp that calls define CATCH_CONFIG_MAIN that decreases the compilation time of the cpp with tests A TON. Also adding some more tests

--- test/src/tests.cpp
+++ test/src/tests.cpp
@@ -1,5 +1,3 @@
-#define CATCH_CONFIG_MAIN
-
 #include "chip8.h"
 #include "catch2/catch.hpp"
 
@@ -11,8 +9,10 @@ void prepare_test(unsigned short op) {
     chip8.ram[513] = op & 0x00FF;
     chip8.pc = 512;
     chip8.game_max_address = 1024;
+    chip8.stack_pointer = 0;
 }
 
+// Clear screen
 TEST_CASE( "00E0 - CLS" ) {
     prepare_test(0x00E0);
     for (int i=0; i<32; i++) {
@@ -35,3 +35,37 @@ TEST_CASE( "00E0 - CLS" ) {
     REQUIRE( all_blank == true );
 }
 
+// Return from a subroutine.
+TEST_CASE( "00EE - RET" ) {
+    prepare_test(0x00EE);
+    chip8.stack_pointer = 1;
+    chip8.stack[0] = 600;
+
+    chip8.runStep();
+
+    REQUIRE( chip8.pc == 602 );
+}
+
+
+// Jump to location nnn
+TEST_CASE( "1nnn - JP addr" ) {
+    unsigned short address = 0x012F;
+    prepare_test(0x1000 | address);
+    
+    chip8.runStep();
+
+    REQUIRE( chip8.pc == address );
+}
+
+// Call subroutine at nnn.
+TEST_CASE( "2nnn - CALL addr" ) {
+    unsigned short address = 0x022F;
+    prepare_test(0x2000 | address);
+    
+    unsigned short old_pc = chip8.pc;
+    chip8.runStep();
+
+    REQUIRE( chip8.stack[0] == old_pc );
+    REQUIRE( chip8.pc == address );
+
+}
